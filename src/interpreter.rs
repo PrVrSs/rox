afbf5ba7b490100bde77d2aad0dbde22acb4dd01
Sergey Reshetnikov <resh.sersh@gmail.com>
2020-08-30 02:06:41 +0700

    update

---
+++ src/interpreter.rs
@@ -0,0 +1,34 @@
+use crate::expr;
+use crate::expr::{Expr, Acceptor};
+use crate::token::Token;
+use crate::error::{Result};
+
+
+#[derive(Debug, Clone)]
+pub enum Object {}
+
+
+#[derive(Debug, Clone)]
+pub struct Interpreter {}
+
+
+impl Interpreter {
+    fn evaluate(&mut self, expr: &Expr) -> Result<Object> {
+        expr.accept(self)
+    }
+}
+
+
+impl expr::Visitor<Result<Object>> for Interpreter {
+    fn visit_assign(&mut self, name: &Token, value: &Expr) -> Result<Object> {
+        unimplemented!()
+    }
+
+    fn visit_variable(&mut self, name: &Token) -> Result<Object> {
+        unimplemented!()
+    }
+
+    fn visit_literal(&mut self, name: &Token) -> Result<Object> {
+        unimplemented!()
+    }
+}
